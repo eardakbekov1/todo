b6ec436ff56a91d366fd62f6abe88f145d102c1f
eardakbekov1 <eardakbekov@gmail.com>
2023-02-22 18:29:28 +0600

    relations added

--- app/Models/Priority.php
+++ app/Models/Priority.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Priority extends Model
 {
-    use HasFactory;
+    protected $fillable = ['id', 'name'];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
+    protected $fillable = ['id', 'description', 'deadline', 'status_id', 'user_id', 'priority_id'];
+
+    public function priority()
+    {
+        return $this->belongsTo(Priority::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
